32b6eb0e0452e6ccbbe0437c580e78049cfd3096
unknown <sujanbhushal2014@gmail.com>
2024-07-28 18:24:35 +0545

    added calendar ui

--- frontend/src/app/(user)/calendar/page.js
+++ frontend/src/app/(user)/calendar/page.js
@@ -1,15 +1,21 @@
 'use client'
 import React from 'react'
-import {Calendar} from "@nextui-org/react";
-import {parseDate} from '@internationalized/date';
-import { getLocalTimeZone, today } from "@internationalized/date";
+import { Calendar } from 'antd';
+import './calendar.css'
 
-const schoolCalendar = () => {
+
+const studentCalendar = () => {
+  const onPanelChange = (value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  }
   return (
-    <div > 
-    <Calendar aria-label="Date (Uncontrolled)" defaultValue={today(getLocalTimeZone())} />
-    </div>
+    <div className='flex flex-col item-center rounded-xl shadow-lg justify-center shadow-blue-900'> 
+    
+     <div className=' flex justify-center text-blue-600 text-2xl font-semibold '>Student Calendar</div> 
+     <div className='pt-4 pl-5'> <Calendar onPanelChange={onPanelChange} /></div>
+      
+      </div>
   )
 }
 
-export default schoolCalendar
+export default studentCalendar
