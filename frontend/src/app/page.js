b919a908b2af94b5edb3c07bc6a89a13898fa9d5
unknown <sujanbhushal2014@gmail.com>
2024-08-03 09:35:57 +0545

    correcting the override error

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,33 +1,20 @@
-// app/page.js
 'use client'
-import  { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 //import CookieService from '../service/cookieService';
-
-
 //const cookieService = CookieService.getService();
+import React from 'react'
+import Login from './login/page'
 
-export default function HomePage() {
-
-    const router = useRouter();
-    //const isAuthenticated = cookieService.getAccessToken();
-
-    useEffect(() => {
-            router.push('/login');
-    }, []);
-
+const homePage = () => {
+         //const isAuthenticated = cookieService.getAccessToken();
+         
     // if (!isAuthenticated) {
     //     return null; // Or a loading spinner
     // }
 
-    return (
-            <div>
-<div className="col p-0" style={{ marginTop: '5px', background: '#f2f2f2', overflowX: 'auto' }}>
-                        <div style={{ padding:'16px' }} className="" ></div>
-                
-                <h1>Welcome to the Home Page</h1>
-                {/* Add more content here */}
-            </div></div>
-    
-    );
+  return (
+    <div><Login/></div>
+  )
 }
+
+export default homePage
+    
